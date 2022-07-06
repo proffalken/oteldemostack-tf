a6b9657a6f52f09f79050f64654cf27e28da1ed6
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-06 23:09:05 +0100

    Add missing variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,64 @@ variable "grafana_cloud_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "heroku_account_email" {
+  default = ""
+  type    = string
+}
+
+variable "heroku_api_key" {
+  default = ""
+  type    = string
+}
+
+variable "backstage_version_number" {
+  default   = "1.0"
+  type      = string
+  sensitive = true
+}
+
+variable "backstage_app_name" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "backstage_region" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "github_client_id" {
+  default = ""
+  type    = string
+}
+
+variable "grafana_uri" {
+  default = ""
+  type    = string
+}
+
+variable "github_client_secret" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+variable "github_pat" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "prometheus_token" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_token" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
