dfef5f397724135b235d19262136a7e164bf2b34
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-06 20:53:02 +0100

    Terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ module "grafana_cloud" {
   source = "https://github.com/proffalken/terraform-grafana-oteldemostack.git"
 
   grafana_cloud_api_key = var.grafana_cloud_api_key
-  grafana_stack_name = "oteldemostack"
-  grafana_region = "eu"
+  grafana_stack_name    = "oteldemostack"
+  grafana_region        = "eu"
 }
