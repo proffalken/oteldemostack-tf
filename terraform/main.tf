6f56049f71b366fdbde08c741f780fb5331da196
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-06 23:06:45 +0100

    Add backstage module

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,14 @@ module "grafana_cloud" {
   grafana_stack_name    = "oteldemostack"
   grafana_region        = "eu"
 }
+
+module "heroku_backstage" {
+  source = "github.com/proffalken/terraform-heroku-oteldemostack-backstage"
+
+  heroku_account_email = var.heroku_account_email
+  heroku_api_key       = var.heroku_api_key
+  backstage_app_name   = "demoapp"
+  backstage_region     = "eu"
+  prometheus_uri       = module.grafana_cloud.prometheus_remote_endpoint
+  prometheus_user      = module.grafana_cloud.prometheus_user_id
+}
