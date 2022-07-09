7f5e58e4614aeb04ec04ff0b081262fe6097c958
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-09 14:48:58 +0100

    ADd prometheus integration to backstage

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,7 @@ module "heroku_backstage" {
   backstage_region         = "eu"
   prometheus_uri           = module.grafana_cloud.prometheus_remote_endpoint
   prometheus_user          = module.grafana_cloud.prometheus_user_id
+  prometheus_viewer_token  = module.grafana_cloud.metrics_viewer_api_key
   github_client_id         = var.github_client_id
   github_client_secret     = var.github_client_secret
   github_pat               = var.github_pat
