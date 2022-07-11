c3cee98ba217d773984720d5ac50d7505bfbba6b
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-11 22:40:32 +0100

    Pass github PAT to grafana module

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ module "grafana_cloud" {
   grafana_cloud_api_key = var.grafana_cloud_api_key
   grafana_stack_name    = "oteldemostack"
   grafana_region        = "eu"
+  github_pat            = var.github_pat
 }
 
 module "heroku_backstage" {
