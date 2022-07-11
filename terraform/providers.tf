96c96c0c8a162712d0cfcca5ad7b67cf7fd0ae0f
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-11 22:11:52 +0100

    The provide is not needed after all

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,15 +6,4 @@ terraform {
       name = "oteldemostack"
     }
   }
-  required_providers {
-    grafana = {
-      source  = "grafana/grafana"
-      version = "~> 1.24.0"
-    }
-  }
-}
-
-provider "grafana" {
-  alias         = "cloud"
-  cloud_api_key = var.grafana_cloud_api_key
 }
